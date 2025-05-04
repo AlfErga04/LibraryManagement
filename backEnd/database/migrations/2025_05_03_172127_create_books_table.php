e2f05a28051f640e2e246ab1aa8593608678741e
yusdi17 <yusdipathudin@gmail.com>
2025-05-04 16:47:54 +0700

    Database migrations dan login regsiter

---
+++ backEnd/database/migrations/2025_05_03_172127_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('image');
+            $table->string('isbn');
+            $table->string('penulis');
+            $table->string('penerbit');
+            $table->date('tahun_terbit');
+            $table->integer('stok')->default(0);
+            $table->text('description');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
